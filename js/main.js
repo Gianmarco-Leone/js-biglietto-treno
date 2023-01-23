7120a40a03d49ea95889c0446c93e863fd727964
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-01-23 15:41:50 +0100

    add conditionals instructions + write in html

--- js/main.js
+++ js/main.js
@@ -13,30 +13,50 @@
 let userTravelKm = parseFloat(prompt("Quanti kilometri devi percorrere?"));
 console.log("I chilometri da percorrere sono " + userTravelKm);
 
+
 // CHIEDERE ALL'UTENTE ETA'
 let userAge = parseInt(prompt("Quanti anni hai?"));
 console.log("L'età dell'utente è " + userAge);
 
+
 // PREZZO BIGLIETTO CALCOLATO IN BASE AI KM
 let price = (userTravelKm * 0.21);
+price = price.toFixed(2);
 console.log("Il prezzo del biglietto è € " + price);
+
 
 // CREAZIONE SCONTO UNDER 18
 const smallDiscountValue = (price * 20) / 100;
 let priceUnder = price - smallDiscountValue;
-console.log("Il valore dello sconto del 20% è €" + smallDiscountValue);
-console.log("Il prezzo con lo sconto del 20% è €" + priceUnder);
+
 
 // CREAZIONE SCONTO OVER 65
 const bigDiscountValue = (price * 40) / 100;
 let priceOver = price - bigDiscountValue;
-console.log("Il valore dello sconto del 40% è €" + bigDiscountValue);
-console.log("Il prezzo con lo sconto del 40% è €" + priceOver)
 
 
 
 // APPLICARE SCONTO UNDER 18
-
-
+if (userAge < 18) {
+    let priceUnder = price - smallDiscountValue;
+    priceUnder = priceUnder.toFixed(2);
+    console.log("Il prezzo scontato per l'under 18 è €" + priceUnder);
+    // Scrivo nell'HTML
+    document.getElementById("price_calculated").innerHTML = ("Usufruendo dello sconto Under 18, il prezzo del suo biglietto è € " + priceUnder);
+}
 // APPLICARE SCONTO OVER 65
-// INDICARE PREZZO CON MAX 2 DECIMALI
+else if (userAge > 65) {
+    let priceOver = price - bigDiscountValue;
+    priceOver = priceOver.toFixed(2);
+    console.log("Il prezzo scontato per l'over 65 è €" + priceOver);
+    // Scrivo nell'HTML
+    document.getElementById("price_calculated").innerHTML = ("Usufruendo dello sconto Over 65, il prezzo del suo biglietto è € " + priceOver);
+}
+// PREZZO PER TUTTI GLI ALTRI UTENTI CHE NON HANNO DIRITTO A SCONTO
+else {
+    let price = (userTravelKm * 0.21);
+    price = price.toFixed(2);
+    console.log("Il prezzo non scontato è €" + price);
+    // Scrivo nell'HTML
+    document.getElementById("price_calculated").innerHTML = ("Il prezzo del suo biglietto è € " + price);
+}
